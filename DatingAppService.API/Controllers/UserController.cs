629096c73ec4c364b4757ef772850bc375cd18ea
marci <kismarczirobi@gmail.com>
2023-07-26 20:28:08 +0200

    Add project skeleton, dbContext, UserController

---
+++ DatingAppService.API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using DatingAppService.API.Data;
+using DatingAppService.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppService.API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class UserController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public UserController(DataContext context)
+		{
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+		{
+			return await _context.Users.ToListAsync();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AppUser>> GetUser(int id)
+		{
+			return await _context.Users.FindAsync(id);
+		}
+
+
+	}
+}
---
+++ DatingAppService.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using DatingAppService.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppService.API.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<AppUser> Users { get; set; }
+	}
+}
