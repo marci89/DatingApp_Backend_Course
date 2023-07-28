df6f354a032e8e170ccc33846765e0fb0c5d70b1
marci <kismarczirobi@gmail.com>
2023-07-28 19:50:18 +0200

    Add AccountController and Register action

---
+++ DatingAppService.API/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using DatingAppService.API.Data;
+using DatingAppService.API.DTOs;
+using DatingAppService.API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DatingAppService.API.Controllers
+{
+	public class AccountController : BaseApiController
+	{
+		private readonly DataContext _context;
+
+		public AccountController(DataContext context)
+		{
+			_context = context;
+		}
+
+
+		[HttpPost("register")]
+		public async Task<ActionResult<AppUser>> Register([FromBody] RegisterDto request)
+		{
+			if (await UserExists(request.UserName))
+				return BadRequest("UserName is taken");
+
+			using var hmac = new HMACSHA512();
+
+			var user = new AppUser
+			{
+				UserName = request.UserName,
+				PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
+				PasswordSalt = hmac.Key
+			};
+
+			_context.Users.Add(user);
+			await _context.SaveChangesAsync();
+
+			return user;
+		}
+
+		private async Task<bool> UserExists(string userName)
+		{
+			return await _context.Users.AnyAsync(x => x.UserName == userName.ToLower());
+		}
+	}
+}
