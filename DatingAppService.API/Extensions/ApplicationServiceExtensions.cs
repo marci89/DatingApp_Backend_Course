23fc210be9757f14f829d3f633b72a2a35cb045b
marci <kismarczirobi@gmail.com>
2023-07-29 15:16:36 +0200

    Add extension methods

---
+++ DatingAppService.API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,27 @@
+﻿using DatingAppService.API.Data;
+using DatingAppService.API.Interfaces;
+using DatingAppService.API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAppService.API.Extensions
+{
+	public static class ApplicationServiceExtensions
+	{
+		public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+		{
+
+			//DbContext
+			services.AddDbContext<DataContext>(options =>
+			{
+				options.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+			});
+
+
+			services.AddCors();
+
+			services.AddScoped<ITokenService, TokenService>();
+
+			return services;
+		}
+	}
+}
--- DatingAppService.API/Program.cs
+++ DatingAppService.API/Program.cs
@@ -1,83 +1,15 @@
 using DatingAppService.API.Data;
-using DatingAppService.API.Interfaces;
-using DatingAppService.API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using DatingAppService.API.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
-#region Swagger
-
-// Add Swagger generation
-builder.Services.AddSwaggerGen(c =>
-{
-	c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
-
-	// Add the security definition for bearer tokens
-	c.AddSecurityDefinition(name: "Bearer", securityScheme: new OpenApiSecurityScheme
-	{
-		Name = "Authorization",
-		Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
-		In = ParameterLocation.Header,
-		Type = SecuritySchemeType.ApiKey,
-		Scheme = "Bearer"
-	});
-
-	// Add the security Requirement for bearer tokens
-	c.AddSecurityRequirement(new OpenApiSecurityRequirement
-	{
-		{
-			new OpenApiSecurityScheme
-			{
-				Name = "Bearer",
-				In = ParameterLocation.Header,
-				Reference = new OpenApiReference
-				{
-					Id = "Bearer",
-					Type =ReferenceType.SecurityScheme
-				}
-			},
-			new List<string>()
-		}
-	});
-});
-
-#endregion
-
-
-//DbContext
-builder.Services.AddDbContext<DataContext>(options =>
-{
-	options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-
-
-builder.Services.AddCors();
-
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-
-//Auth
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuerSigningKey = true,
-						IssuerSigningKey = new SymmetricSecurityKey(Encoding
-							.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-						ValidateIssuer = false,
-						ValidateAudience = false
-					};
-				});
-
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddSwaggerServices();
+builder.Services.AddIdentityServices(builder.Configuration);
 
 
 
